fd8f1edbc9e99db03bbd11f61af72c349851b648
Cris <crisguycabs@gmail.com>
2021-09-02 21:56:25 -0500

    Final

---
+++ FormPrac1/FormPrac1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormPrac1
+{
+    public partial class Form1 : Form
+    {
+        public SqlConnection con = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DemoDB;Integrated Security=True");
+        SqlCommand cmd;
+        SqlDataReader dr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con.Open();
+            MessageBox.Show("Conexion creada con exito!");
+            con.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            string query = "Select * from Alumno Where Edad>15";
+            cmd = new SqlCommand(query, con);
+
+            //Set the SqlDataAdapter object
+            SqlDataAdapter dAdapter = new SqlDataAdapter(cmd);
+
+            //define dataset
+            DataSet ds = new DataSet();
+
+            //fill dataset with query results
+            dAdapter.Fill(ds);
+
+            dataGridView1.ReadOnly = true;
+            dataGridView1.DataSource = ds.Tables[0];
+
+            con.Close();
+        }
+    }
+}
